4f65933b73d7fe52b0974d4e7e6a2b587b483e23
Jitthapong Arjsalee <jitthapong@vtec-system.com>
2022-01-24 14:41:54 +0700

    update

--- lib/printer.dart
+++ lib/printer.dart
@@ -7,10 +7,15 @@ class Printer {
 
   static Future<String> printBill(
       String printerName, int tcpPort, String content) async {
+    content = _customizeContent(content);
+    return await _channel.invokeMethod("printBill",
+        {'printerName': printerName, 'tcpPort': tcpPort, 'content': content});
+  }
+
+  static _customizeContent(String content) {
     content = content.replaceAll('70mm', '144mm');
     content = content.replaceAll(
         '<hr/>', '<hr style="border-top:2px solid black;"/>');
-    return await _channel.invokeMethod("printBill",
-        {'printerName': printerName, 'tcpPort': tcpPort, 'content': content});
+    return content;
   }
 }
